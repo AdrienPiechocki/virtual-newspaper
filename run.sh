1ed56ba8a6a6cf9b23e3b3cbfc8a63cf8b3c6db5
AdrienPiechocki <adrien.piechocki@proton.me>
2026-06-18 09:53:23 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ source .venv/bin/activate
 
 python -m scripts.articles.rss_reader https://www.lemonde.fr/rss/en_continu.xml https://www.franceinfo.fr/titres.rss
 python -m scripts.linkedin.linkedin_scraper
-python -m scripts.steam.steam_trending --no-cache
+python -m scripts.steam.steam_trending --clear-cache
 python -m scripts.forecast.weather_forecast
 
 DATE=$(date +%Y-%m-%d)
@@ -16,10 +16,10 @@ OUTPUT_FILE="output/LHebdoDuNerd-${DATE}.pdf"
 
 python generate_newspaper.py \
     --data-dir data \
-    --output "$OUTPUT_FILE" \
+    --output "output/Journal.pdf" \
     --masthead "L'Hebdo du Nerd"
 
 curl --fail \
      -u "$NEXTCLOUD_USER:$NEXTCLOUD_APP_PASSWORD" \
-     -T "$OUTPUT_FILE" \
+     -T "output/Journal.pdf" \
      "https://${NEXTCLOUD_DOMAIN}/remote.php/dav/files/$NEXTCLOUD_USER/L%27Hebdo%20du%20Nerd/$(basename "$OUTPUT_FILE")"
