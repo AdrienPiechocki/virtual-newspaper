fd37d97bf567113985f0ce9e67b2c456084e3faf
AdrienPiechocki <adrien.piechocki@proton.me>
2026-06-18 10:40:05 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -23,3 +23,23 @@ curl --fail \
      -u "$NEXTCLOUD_USER:$NEXTCLOUD_APP_PASSWORD" \
      -T "output/Journal.pdf" \
      "https://${NEXTCLOUD_DOMAIN}/remote.php/dav/files/$NEXTCLOUD_USER/L%27Hebdo%20du%20Nerd/$(basename "$OUTPUT_FILE")"
+     
+# Garder uniquement les 5 derniers journaux sur Nextcloud
+NEXTCLOUD_DIR_URL="https://${NEXTCLOUD_DOMAIN}/remote.php/dav/files/$NEXTCLOUD_USER/L%27Hebdo%20du%20Nerd/"
+
+curl -s -u "$NEXTCLOUD_USER:$NEXTCLOUD_APP_PASSWORD" \
+     -X PROPFIND \
+     -H "Depth: 1" \
+     "$NEXTCLOUD_DIR_URL" \
+  | grep -oP '(?<=<d:href>)[^<]+' \
+  | grep '\.pdf' \
+  | sort \
+  | head -n -5 \
+  | while read -r filepath; do
+      filename=$(basename "$filepath")
+      decoded_filename=$(python3 -c "import urllib.parse,sys; print(urllib.parse.unquote(sys.argv[1]))" "$filename")
+      echo "Suppression de l'ancien journal : $decoded_filename"
+      curl -s -u "$NEXTCLOUD_USER:$NEXTCLOUD_APP_PASSWORD" \
+           -X DELETE \
+           "https://${NEXTCLOUD_DOMAIN}/remote.php/dav/files/$NEXTCLOUD_USER/L%27Hebdo%20du%20Nerd/$filename"
+    done
